bfc2585c235660bfc5d47ce0c6195998acd0ccad
hanshir <hanshirlukku@gmail.com>
2021-01-30 14:41:00 +0530

    bloglist component added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
@@ -12,12 +13,7 @@ const Home = () => {
   ]);
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList />
     </div>
   );
 };
