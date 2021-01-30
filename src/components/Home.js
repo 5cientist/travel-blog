49b1035cfea4c2cdb8abc7bf45a7e251ff45b129
hanshir <hanshirlukku@gmail.com>
2021-01-30 15:03:38 +0530

    props author filter

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,6 +14,10 @@ const Home = () => {
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs !!!" />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "mario")}
+        title="mario's blogs"
+      />
     </div>
   );
 };
