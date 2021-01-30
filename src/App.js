ae2abba602ce6d74168bd53537c168fe60547c3a
hanshir <hanshirlukku@gmail.com>
2021-01-30 19:27:12 +0530

    Router link added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./components/Create";
 
 function App() {
   return (
@@ -9,8 +10,11 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
             </Route>
           </Switch>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,20 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>The Travel Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a
-          href="/create"
-          // style={{
-          //   color: "white",
-          //   backgroundColor: "#f1356d",
-          //   borderRadius: "8px",
-          // inline CSS in reactjs
-          // }}
+        <Link to="/">Home</Link>
+        <Link
+          to="/create"
+          style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px",
+          }}
         >
           New Blog
-        </a>
+        </Link>
       </div>
     </nav>
   );
