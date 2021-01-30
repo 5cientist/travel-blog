3002bc0539af886ed5ad88f309c14efee7befb10
hanshir <hanshirlukku@gmail.com>
2021-01-30 14:30:03 +0530

    added map

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h1>{blog.title}</h1>
+          <p>written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,20 @@
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>the Travel Blog</h1>
+      <h1>The Travel Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+        <a
+          href="/create"
+          // style={{
+          //   color: "white",
+          //   backgroundColor: "#f1356d",
+          //   borderRadius: "8px",
+          // inline CSS in reactjs
+          // }}
+        >
+          New Blog
+        </a>
       </div>
     </nav>
   );
