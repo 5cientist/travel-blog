5022f7a10e54cdedfdcc96fe805fb12edf2148c6
hanshir <hanshirlukku@gmail.com>
2021-01-30 21:35:58 +0530

    new blog controll input added

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs, title }) => {
   // const blogs = props.blogs;
   // const title = props.title;
@@ -6,8 +8,10 @@ const BlogList = ({ blogs, title }) => {
       <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>written by {blog.author}</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>written by {blog.author}</p>
+          </Link>
         </div>
       ))}
     </div>
--- src/components/Create.js
+++ src/components/Create.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog Author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>add blog</button>
+      </form>
     </div>
   );
 };
