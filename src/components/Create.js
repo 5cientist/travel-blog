85954a253ab1ad375d00ea95abc5e90a9ffbcfe6
hanshir <hanshirlukku@gmail.com>
2021-01-30 22:05:11 +0530

    post request

--- src/components/Create.js
+++ src/components/Create.js
@@ -4,12 +4,20 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [ispending, setIspending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-
-    console.log(blog);
+    setIspending(true);
+    fetch("http://localhost:8000/blogs/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setIspending(false);
+    });
   };
 
   return (
@@ -34,7 +42,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>add blog</button>
+        {!ispending && <button>add blog</button>}
+        {ispending && <button>Adding blog....</button>}
       </form>
     </div>
   );
