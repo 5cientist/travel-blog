8d8776181e07aa2836162a5df10c2c3cbec19d1f
hanshir <hanshirlukku@gmail.com>
2021-01-30 19:41:38 +0530

    useEffect cleanup

--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -6,8 +6,9 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortCont = new AbortController();
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then((res) => {
           if (!res.ok) {
             // error coming back from server
@@ -21,11 +22,17 @@ const useFetch = (url) => {
           setError(null);
         })
         .catch((err) => {
-          // auto catches network / connection error
-          setIsPending(false);
-          setError(err.message);
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            // auto catches network / connection error
+            setIsPending(false);
+            setError(err.message);
+          }
         });
     }, 1000);
+    // abort the fetch
+    return () => abortCont.abort();
   }, [url]);
 
   return { data, isPending, error };
