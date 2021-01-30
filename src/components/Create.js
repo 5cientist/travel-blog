d274606e6c7c963001370ddd42b35e88009d4767
hanshir <hanshirlukku@gmail.com>
2021-01-30 22:10:05 +0530

    redirect added

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
   const [ispending, setIspending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,6 +19,8 @@ const Create = () => {
     }).then(() => {
       console.log("new blog added");
       setIspending(false);
+      // history.go(-1);
+      history.push("/");
     });
   };
 
